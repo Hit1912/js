dc25ed7d0784de7da06e2c3387b03313f17ed265
hit1912 <hitdungrani39@gmail.com>
2026-01-23 12:40:47 +0530

    update code

--- controllflow.js
+++ controllflow.js
@@ -108,44 +108,171 @@ else if (condition2){
 // }
 
 // Important Example:-
+// let age, weight, height, gender;
+ 
+// age = prompt("enter age : ");
+// weight = prompt("enter weight : " );
+// height = prompt("enter height : ");
+// gender = prompt("enter gender : ");
+ 
+// if(age >= 18 && age <= 25 && weight <= 70 && height >= 5 && height <= 6 && gender === "male"){
+//     console.log("he is fit.");
+// }
+// else if(age >= 18 && age <= 25 && weight <= 50 && height >= 4.5 && height <= 5.5 && gender === "female"){
+//     console.log("she is fit.")
+// }
+// else{
+//     console.log("you are not fit.")
+// }
 
 
 // Switch Case Statement
-switch ("BOGO")  // value ---> Case Value sathe match kaeshe ane code run thase
-{
-    case "First50":
-        console.log("You got 50% off");
-        break;
-    case "BOGO":
-        console.log("Buy One Get One Free");
-        break;
-    case "FREESHIP":
-        console.log("Free Shipping on your order");
-        break;
-    default:
-        console.log("Invalid Promo Code");
-}
+// switch ("BOGO")  // value ---> Case Value sathe match kaeshe ane code run thase
+// {
+//     case "First50":
+//         console.log("You got 50% off");
+//         break;
+//     case "BOGO":
+//         console.log("Buy One Get One Free");
+//         break;
+//     case "FREESHIP":
+//         console.log("Free Shipping on your order");
+//         break;
+//     default:
+//         console.log("Invalid Promo Code");
+// }
 
 
 // Day Message System
 
 // Take input from user
-let day = prompt("Enter day (Monday, Friday, Saturday, Sunday)");
+// let day = prompt("Enter day (Monday, Friday, Saturday, Sunday)");
 
 // Switch Case
-switch (day) {
-    case "Monday":
-        console.log("Start of the work week");
-        break;
-    case "Friday":
-        console.log("Weekend is coming 🎉");
-        break;
-    case "Saturday":
-    case "Sunday":
-        console.log("It's a holiday 😎");
-        break;
-    default:
-        console.log("Invalid day");
+// switch (day) {
+//     case "Monday":
+//         console.log("Start of the work week");
+//         break;
+//     case "Friday":
+//         console.log("Weekend is coming 🎉");
+//         break;
+//     case "Saturday":
+//     case "Sunday":
+//         console.log("It's a holiday 😎");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+//Early Return Pattern
+// function score(value){
+//     if (value >90){
+//         return "Value is more than 90";
+//     } else if (value >80){
+//         return "Value is more than 80";
+//     } else if (value >70){
+//         return "Value is more than 70";
+//     }else if (value >60){
+//         return "Value is more than 60";
+//     } else {
+//         return "Value is less than or equal to 60";
+//     }
+// }
+// score(100);
+// console.log(score(95));
+
+// function score1(value){
+//     if (value >90) return "Value is more than 90";
+//     if (value >80) return "Value is more than 80";      
+//     if (value >70) return "Value is more than 70";  
+//     if (value >60) return "Value is more than 60";
+//     return "Value is less than or equal to 60";
+// }
+// score1(85);
+// console.log(score1(85));
+
+/* ---> Other Example <--- */
+// function login(username, password){
+//     if (username !== "admin") return "Invalid Username";
+//     if (password !== "1234") return "Invalid Password";
+//     return "Login Successful";
+// }
+
+// console.log(login("admin", "1234"));   // Login Successful
+// console.log(login("user", "1234"));    // Invalid Username
+
+/* ---> Exercises 1 <--- */
+
+let x = 2;
+switch(x){
+   case 2:
+      console.log("Two");
+   case 3:
+      console.log("Three");
 }
 
 
+/*---> Exercise 2 <--- */
+// function getGrade(score) {
+//   switch (true) {
+//     case (score >= 90 && score <= 100):
+//       return "A+";
+//     case (score >= 80 && score <= 89):
+//       return "A";
+//     case (score >= 70 && score <= 79):
+//       return "B";
+//     case (score >= 60 && score <= 69):
+//       return "C";
+//     case (score >= 33 && score <= 59):
+//       return "D";
+//     case (score >= 0 && score <= 32):
+//       return "Fail";
+//     default:
+//       return "Invalid Marks";
+//   }
+// }
+
+// console.log(getGrade(85));  // Output: A
+
+
+/*---> Exercise 3 <--- */
+function game(player1, player2) {
+  switch (player1) {
+    case "rock":
+      switch (player2) {
+        case "scissors":
+          return "rock";
+        case "paper":
+          return "paper";
+        default:
+          return "Invalid Input";
+      }
+
+    case "paper":
+      switch (player2) {
+        case "rock":
+          return "paper";
+        case "scissors":
+          return "scissors";
+        default:
+          return "Invalid Input";
+      }
+
+    case "scissors":
+      switch (player2) {
+        case "paper":
+          return "scissors";
+        case "rock":
+          return "rock";
+        default:
+          return "Invalid Input";
+      }
+
+    default:
+      return "Invalid Input";
+  }
+}
+
+console.log(game("rock", "scissors"));   // Output: rock
+console.log(game("paper", "rock"));      // Output: paper
+console.log(game("scissors", "paper"));  // Output: scissors
