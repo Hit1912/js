dab5c7f11f858e20b21f8e22dabb29f586fde8d1
hit1912 <hitdungrani39@gmail.com>
2026-03-03 12:14:33 +0530

    Toaster make

--- Toaster/Toaster.js
+++ Toaster/Toaster.js
@@ -1,49 +1,85 @@
-let d = 20;
-function abc() {
-    console.log(d);
-}
-function def(){
-    let d = 25;
-    abc();
-}
-def();
-//clg ans -->
+// let d = 20;
+// function abc(){
+//     console.log(d);
+// }
+// function def(){
+//     let d = 25;
+//     abc();
+//     console.log(d);
+// }
+// def();
+// // clg ans --> 20 
 
-function parent (){
-    let a = 12;
-    return function(){
-        console.log(a);
+// function parent(){
+//     let a = 12;
+//     return function (){
+//         console.log(a);
+//     };
+// }
+// let fnc1 = parent();
+// // console.log(fnc1());
+// fnc1();
+
+// // counter
+// function counter(){
+//     let a = 0;
+//     return function(){
+//         a ++;
+//         console.log(a);
+//     };
+// }
+// let fnc = counter();
+// // console.log(fnc());
+// // console.log(fnc());
+// fnc();
+// fnc();  
+// fnc();
+// fnc();
+// fnc();
+
+// let fnc2 = counter();
+// fnc2();
+// fnc2();
+// fnc2();
+// fnc2();
+// fnc2();
+
+// create toaster
+function createToaster(config){
+  let toast = document.querySelector(".toast");
+
+    return function(noti){
+        let div = document.createElement("div");
+        div.className = `toaster  ${config.theme === "dark"? "bg-gray-800 text-white" : "bg-white text-red-950 border-2 border-red-950"} 
+         px-6 py-3 inline-block top-4 right-4 rounded-md pointer-event-none`;
+    
+      toast.className = `${config.positionX === "left"? "left-10":"right-10"}
+      ${config.positionY === "bottom"? "bottom-10":"top-10"}`
+
+
+        div.textContent = noti;
+
+        toast.appendChild(div);
+
+        setTimeout(()=>{
+            toast.removeChild(div);
+        },config.duration * 1000);
     };
 }
-let fnc1 = parent()();
-console.log(fnc1);
+ 
+let toaster = createToaster({
+positionX: "left",
+positionY: "bottom",
+theme: "light",
+duration: 3,
+});
 
 
-//Counter
-function counter(){
-    let a = 0;
-    return function(){
-        a++;
-        console.log(a);
-    };
-}
-
-let fnc = counter();
-fnc(); //1
-fnc(); //2 
-fnc(); //3
-fnc(); //4
-fnc(); //5
-
-let fnc2 = counter();
-fnc2();
-fnc2();
-fnc2();
-fnc2();
-fnc2();
-
-
-
+toaster("welcome to Dashboard");
+setTimeout(()=>{
+    toaster("You have two notification");
+},2000);
+toaster("Product upload Successfully");
 
 
 
